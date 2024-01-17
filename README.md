d9eaae9178b38ba13fc161f7221b2cc0c71d52dc
1BObC <bob.cocksedge@gmail.com>
2024-01-17 13:01:38 +0000

    OK so new file  helloworld works! So now look at nextstep Maybe put all code from index p h p file and try it? - OK

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ First exercise accessing MySQL from DOM
 "Set boiler plate and associated files"
 "Error warning:  The character encoding of the document was not declared, so the encoding was guessed from content. The character encoding needs to be declared in the Content-Type HTTP header or using a byte order mark. Will invstigate!"
 "Notes made on DOM to DB1 doc in MySQLshowInTerminal of actions to try to resolve the error warining"
+"OK so new file express1 php with copy of boiler plate and helloworld works! So now look at next
+step Maybe put all code from index php file and try it? - OK"
---
+++ express1.php
@@ -0,0 +1,39 @@
+<!-- Expressions 3:08:00 -->
+<!-- Copy of hello world in express.php in bobsWebDemos --> 
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]>      <html class="no-js"> <![endif]-->
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Dr Chuck PHP</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="style.CSS">
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        <?php
+        ini_set('display_errors', 1); 
+        //phpinfo();
+        ?> 
+ 
+ <h1>Copy of First attempt with PHP</h1>
+<p><?php
+            echo 
+            "Hello to you!";
+            echo "<br>" ;
+?>      
+
+<?php
+    echo"<p>Page End</p>";
+    ?>
+    
+        <script src="" async defer></script>
+    </body>
+</html>
